a34a567b39e0f50bee89010f86768d7864d9ca00
Fernando Roldan <doskoy93@gmail.com>
2019-11-25 16:53:31 +0100

    Añadidos modificadores y consultores de atributos. closes #14 closes #15

--- src/bicicletas.py
+++ src/bicicletas.py
@@ -9,6 +9,33 @@ class Bicicleta:
     def __init__(self, id, fecha_alta, ultimos_usuarios):
         self.id = id
         self.fecha_alta = fecha_alta
+        self.__disponible = True
         self.__ocupada = False
         self.__ultimos_usuarios = [None]
-    
+
+    def set_ocupada(self, ocupada):
+        if self.__ocupada == ocupada:
+            return "Esta bicicleta ya se encuentra en el estado que quieres asignar "
+        else:
+            self.__ocupada = ocupada
+            return "Estado cambiado correctamente :D"
+
+    def get_ocupada(self):
+        return self.__ocupada
+
+    def add_ultimo_usuario(self, usuario):
+        self.__ultimos_usuarios.append()
+        return "Añadido usuario al registro de la bicicleta"
+
+    def get_ultimos_usuarios(self):
+        return self.__ultimos_usuarios
+
+    def set_disponible(self, disponible):
+        if self.__disponible == ocupada:
+            return "Esta bicicleta ya se encuentra en el estado que quieres asignar "
+        else:
+            self.__disponible = ocupada
+            return "Estado cambiado correctamente :D"
+
+    def get_disponible(self):
+        return self.__disponible
